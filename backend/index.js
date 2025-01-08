aa322ba96c9e51338c8481d54eac24dd391f6b6c
Yashika118 <134385059+Yashika118@users.noreply.github.com>
2025-01-08 07:27:01 +0530

    corsss

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,13 @@ app.use("/api/auth",authRouter);
 app.use("/api/book",bookRouter);
 app.use("/api/transaction",transactionRouter);
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://library-management-system-bx19cffw8-yashikas-projects-2f67044d.vercel.app');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    next();
+});
 
 
 // if(process.env.NODE_ENV==="production"){
