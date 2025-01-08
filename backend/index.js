04b1c357cea15e5665694fb1a228fff268fe52df
Yashika118 <134385059+Yashika118@users.noreply.github.com>
2025-01-08 07:22:56 +0530

    test

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,10 @@ const __dirname=path.resolve();
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(cors())
+app.use(cors({
+    origin:"http://localhost:5173",
+    credentials:true,
+}))
 
 app.use("/api/auth",authRouter);
 app.use("/api/book",bookRouter);
