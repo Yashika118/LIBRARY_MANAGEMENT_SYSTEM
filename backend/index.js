eee9e77ff0f66ab12c3713770ebdc045980d92fa
Yashika118 <134385059+Yashika118@users.noreply.github.com>
2025-01-08 07:19:15 +0530

    fixxed cors

--- backend/index.js
+++ backend/index.js
@@ -22,10 +22,7 @@ const __dirname=path.resolve();
 app.use(cookieParser());
 app.use(express.json());
 
-app.use(cors({
-    origin:"http://localhost:5173",
-    credentials:true,
-}))
+app.use(cors())
 
 app.use("/api/auth",authRouter);
 app.use("/api/book",bookRouter);
